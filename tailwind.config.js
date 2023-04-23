9dedf89dd005459a1ba91988adaf1db107273502
Maicon Lima <mai.ky.3000@gmail.com>
2023-04-23 11:59:27 -0300

    feat: create assets and update tailwind config theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    colors: {
+      'brand-green': '#1DC763',
+      'brand-blue': '#0095FF',
+      'brand-violet': '#4743FF',
+      'brand-red': '#FF0100',
+      'brand-light': '#66BFFF',
+      'neutral-light': '#E4EBFB',
+      'neutra-clear': '#D0DAEF',
+      'neutral-cloudy': '#7183A6',
+      'neutral-dark': '#354564',
+      'neutral-darkest': '#04183E',
+      'light1': '#EFF2F9',
+      'light2': '#F5F5F5',
+      'dark1': '#1D1B1B',
+      'white': '#FFFFFF',
+    },
+    fontFamily: {
+      'sans': ['roboto', 'sans-serif'],
+      'serif': ['poppins', 'serif'],
+    }
   },
   plugins: [],
 }
